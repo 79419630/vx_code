1799297f939670d6da78241b97d5f7f9072f02bc
知秋 <79419630@qq.com>
2023-07-15 20:21:26 +0800

    背景

---
+++ src/components/LoginData.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="from_data">
+    <div class="from_data_box">
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="60px"
+        size="small"
+        hide-required-asterisk
+      >
+        <el-form-item label="帐 号:" prop="user">
+          <el-input v-model="ruleForm.user"></el-input>
+        </el-form-item>
+        <el-form-item label="密 码:" prop="password">
+          <el-input type="password" v-model="ruleForm.password"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        user: "",
+        password: "",
+      },
+      rules: {
+        user: [{ required: true, message: "请输入帐号", trigger: "blur" }],
+        password: [],
+      },
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+::v-deep .el-form-item__label {
+  color: white;
+  font-weight: 550;
+}
+</style>
+<style></style>
--- src/views/HomeLogin.vue
+++ src/views/HomeLogin.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="home">
     <div class="login_home">
-      <div class="login_content">123</div>
+      <div class="home_img"><img src="aa" alt="" /></div>
+      <div class="login_content">
+        <login-data></login-data>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import LoginData from "../components/LoginData.vue";
+export default {
+  components: {
+    LoginData,
+  },
 
-export default {};
+  data() {
+    return {};
+  },
+};
 </script>
 <style lang="scss" scoped>
 .home {
   height: 100%;
-  background: url("../assets/img/loding.jpg") no-repeat, center;
+  background: url("../assets/img/loding.jpg");
+
   background-size: cover;
+  background-position: -30% center;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   .login_home {
+    width: 300px;
+    height: 450px;
+    border: 3px beige solid;
+    border-radius: 10px;
+
+    .login_content {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
   }
 }
 </style>
