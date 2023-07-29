3a2431fa361dc92365edd9a81e58609c9989c5c5
知秋 <79419630@qq.com>
2023-07-29 23:41:42 +0800

    修改main

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -4,8 +4,60 @@
       <el-container>
         <el-header>Header</el-header>
         <el-container>
-          <el-aside width="200px">Aside</el-aside>
-          <el-main>Main</el-main>
+          <el-aside width="200px">
+            <div class="aside">
+              <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+                <el-radio-button :label="false">展开</el-radio-button>
+                <el-radio-button :label="true">收起</el-radio-button>
+              </el-radio-group>
+              <el-menu
+                default-active="1-4-1"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                :collapse="isCollapse"
+                @select="handleSelect"
+              >
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span slot="title">导航一</span>
+                  </template>
+                  <el-menu-item-group>
+                    <span slot="title">分组一</span>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="1-4">
+                    <span slot="title">选项4</span>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-menu-item index="2">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                  <i class="el-icon-document"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航四</span>
+                </el-menu-item>
+              </el-menu>
+            </div>
+          </el-aside>
+          <el-main>
+            <div class="mai">
+              <div v-if="currentMenu === '1-1'">你好</div>
+              <div v-if="currentMenu === '1-2'">你好啊</div>
+              <div v-if="currentMenu === '1-3'">你好哈哈</div>
+            </div>
+          </el-main>
         </el-container>
       </el-container>
     </div>
@@ -13,7 +65,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isCollapse: true,
+      currentMenu: "",
+    };
+  },
+  methods: {
+    handleSelect(Index) {
+      this.currentMenu = Index;
+      console.log(Index);
+    },
+  },
+};
 </script>
-
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
 <style lang="scss" scoped></style>
