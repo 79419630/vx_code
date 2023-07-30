20f3a28b7c4a8bf90379c20cc3a731bbda5ddd21
知秋 <79419630@qq.com>
2023-07-31 01:14:03 +0800

    修改菜单导航栏bug

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -23,65 +23,43 @@
           </div></div
       ></el-header>
       <el-container>
-        <el-aside width="220px">
+        <el-aside :width="isCollapse ? '66px' : '220px'">
           <el-menu
-            default-active="$router.push"
+            :default-active="currentMenu"
             @select="handleSelect"
             :collapse="isCollapse"
-            background-color="#1f2b35"
-            text-color="#fff"
+            background-color="#fff"
+            text-color="#000"
             active-text-color="#409eff"
             :collapse-transition="false"
-            router="true"
+            :router="true"
           >
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title"></span>
-              </template>
-              <el-menu-item-group>
-                <span slot="title">分组一</span>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <span slot="title">选项4</span>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title"></span>
-              </template>
-              <el-menu-item-group>
-                <span slot="title">分组一</span>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <span slot="title">选项4</span>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
+            <el-menu-item index="/home">
+              <i class="el-icon-s-home"></i>
+              <span style="margin-right: 27px">首页</span>
             </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
+            <el-submenu
+              v-for="(item, index) in menuItems"
+              :key="index"
+              :index="item.route"
+            >
+              <template slot="title">
+                <i :class="item.icon"></i>
+                <span>{{ item.title }}</span>
+              </template>
+              <el-menu-item-group v-if="item.children">
+                <el-menu-item
+                  v-for="(items, index) in item.children"
+                  :key="index"
+                  :index="items.route"
+                  >{{ items.title }}
+                </el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
           </el-menu>
         </el-aside>
 
-        <el-main>Main</el-main>
+        <el-main>main</el-main>
       </el-container>
     </el-container>
   </div>
@@ -93,13 +71,48 @@ export default {
     return {
       isCollapse: true,
       currentMenu: "",
+      menuItems: [
+        {
+          title: "系统管理",
+          icon: "el-icon-setting",
+          route: "/",
+          role: "both",
+          children: [
+            {
+              title: "用户列表",
+              route: "/users/list",
+            },
+            {
+              title: "添加用户",
+              route: "/users/add",
+            },
+          ],
+        },
+        {
+          title: "个人资料",
+          icon: "el-icon-s-custom",
+          route: "/profile",
+          role: "user",
+          children: [
+            {
+              title: "用户列表",
+              route: "/users/list",
+            },
+            {
+              title: "添加用户",
+              route: "/users/add",
+            },
+          ],
+        },
+      ],
     };
   },
   methods: {
     handleSelect(index) {
-      this.currentMenu = index;
       console.log(index);
+      this.currentMenu = index;
     },
+
     isShow() {
       this.isCollapse = !this.isCollapse;
     },
@@ -110,15 +123,19 @@ export default {
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   min-height: 400px;
 }
-.el-menu {
-  height: 100vh;
-}
+
 .el-header {
   padding: 0px !important;
 }
 /* 侧边栏样式*/
 .el-aside::-webkit-scrollbar {
   display: none;
+}
+.el-aside {
+  background-color: #fff;
+}
+.el-menu {
+  border-right: 0px !important;
 }
 </style>
 <style lang="scss" scoped>
@@ -132,6 +149,7 @@ export default {
     align-items: center;
     height: 100%;
     background-color: #fff;
+
     .title {
       width: 230px;
       display: flex;
